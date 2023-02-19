34b8fa7393aca5e9ecb30a023eb6a004d459511b
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-19 09:56:57 -0300

    fix capital

--- src/strats/ratesmmts.py
+++ src/strats/ratesmmts.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
                                             vol_window=90,
                                             vol_target=0.1,
                                             resample_freq="B",
-                                            capital=1000000)
+                                            capital=20000000)
 
         target_dict["portfolio"] = portfolio_df
         output_path = os.path.join(os.getcwd(), "src", "data", "outputs", sysname, "{}.pickle".format(sysname))
