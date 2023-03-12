0cb2b3d7e4803eb8c5f4525a0b17de1fec2082cf
dcuoliveira <dan.cunhaa@gmail.com>
2023-03-12 16:55:36 -0300

    fix diagnostics

--- src/portfolio_tools/Diagnostics.py
+++ src/portfolio_tools/Diagnostics.py
@@ -17,12 +17,15 @@ class Diagnostics(object):
     def default_metrics(self,
                         sysname):
         
+        self.capital_ret = self.portfolio_df["capital ret"].loc[self.portfolio_df["capital ret"] != 0]
+        self.daily_pnl = self.portfolio_df["daily pnl"].loc[self.portfolio_df["daily pnl"] != 0]
+
         if "capital ret" in self.portfolio_df.columns:
-            self.portfolio_df["cum ret"] = (1 + self.portfolio_df["capital ret"]).cumprod()
+            self.portfolio_df["cum ret"] = (1 + self.capital_ret).cumprod()
             self.portfolio_df["drawdown"] = self.portfolio_df["cum ret"] / self.portfolio_df["cum ret"].cummax() - 1
-            self.sharpe = self.portfolio_df["capital ret"].mean() / self.portfolio_df["capital ret"].std() * np.sqrt(253)
+            self.sharpe = self.capital_ret.mean() / self.capital_ret.std() * np.sqrt(252)
             self.drawdown_max = self.portfolio_df["drawdown"].min() * 100
-            self.vol_ann = self.portfolio_df["capital ret"].std() * np.sqrt(252) * 100
+            self.vol_ann = self.capital_ret.std() * np.sqrt(252) * 100
 
             self.summary = "{}: \nSharpe: {} \nDrawdown: {} \nVolatility: {} \n".format(
                 sysname.upper(), round(self.sharpe, 2), round(self.drawdown_max, 1), round(self.vol_ann, 1)
@@ -33,12 +36,12 @@ class Diagnostics(object):
         if "daily pnl" in self.portfolio_df.columns:
             self.portfolio_df["cum pnl"] = self.portfolio_df["daily pnl"].cumsum()
             # self.portfolio_df["drawdown"] = self.portfolio_df["cum pnl"] / self.portfolio_df["cum pnl"].cummax()
-            self.money_sharpe = self.portfolio_df["daily pnl"].mean() / self.portfolio_df["daily pnl"].std() * np.sqrt(252)
+            self.money_sharpe = self.daily_pnl.mean() / self.daily_pnl.std() * np.sqrt(252)
             # self.money_drawdown_max = self.portfolio_df["drawdown"].replace(np.inf, np.nan).replace(-np.inf, np.nan).min()
-            self.money_vol_daily = self.portfolio_df["daily pnl"].std()
+            self.money_vol_daily = self.daily_pnl.std()
 
             self.money_summary = "{}: \nSharpe: {} \nDaily Mean: {} \nDaily Volatility: {} \n".format(
-                sysname.upper(), round(self.money_sharpe, 2), usdollars_format(self.portfolio_df["daily pnl"].mean()), usdollars_format(self.money_vol_daily)
+                sysname.upper(), round(self.money_sharpe, 2), usdollars_format(self.daily_pnl.mean()), usdollars_format(self.money_vol_daily)
             )    
         else:
             print('There is no columns named "daily pnl" in the "portfolio_df" object')
@@ -103,7 +106,7 @@ class Diagnostics(object):
         ax = sns.histplot(data=self.portfolio_df["daily pnl"], palette="deep")
         ax.annotate(
             "{}: \nAvg. PnL: {} \n1 Std. PnL: {}\n".format(
-                sysname.upper(), usdollars_format(self.portfolio_df["daily pnl"].mean()), usdollars_format(self.portfolio_df["daily pnl"].std())
+                sysname.upper(), usdollars_format(self.daily_pnl.mean()), usdollars_format(self.daily_pnl.std())
             ),
             xy=(0.2, 0.8),
             xycoords="axes fraction",
